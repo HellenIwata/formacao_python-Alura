ec7b540980ce08e95ad1379d00236b1aafa11b28
Hellen Iwata <hellen.iwata001@gmail.com>
2024-01-22 03:20:30 -0300

    Adicionando o diretório criado para adicionar os exercícios práticos do módulo do curso de orientação a objetos

---
+++ projetos_exercitando/hora_da_pratica/oo/classe_musica.py
@@ -0,0 +1,9 @@
+class Musica:
+    nome = ''
+    artista_ou_banda = ''
+    estilo_musical = ''
+
+musica_estilo1 = Musica()
+musica_estilo1.nome = 'Os meninos estão com os pacotes'
+musica_estilo1.artista_ou_banda = 'MC PH'
+musica_estilo1.estilo_musical = 'Funk'

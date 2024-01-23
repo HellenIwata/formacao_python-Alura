483dc53ad6838eaab7f230925c6e0e9ed80964c6
Hellen Iwata <hellen.iwata001@gmail.com>
2024-01-22 23:38:38 -0300

    Adicionando arquivo modificado de acordo com o avanço das aulas (adicionando métodos especiais e metodos criados por mim)

--- 2-python_oo/sabor_express_oo/modelos/modelos.py
+++ 2-python_oo/sabor_express_oo/modelos/modelos.py
@@ -1,9 +1,26 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        print('Nome | Categoria | Status')
+        for restaurante in Restaurante.restaurantes:
+            
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            
 
-restaurante_sushi_iwata = Restaurante()
-restaurante_sushi_iwata.nome = 'Iwata Sushi'
-restaurante_sushi_iwata.categoria = 'Japones'
-restaurante_jorge_boteco = Restaurante()
+
+restaurante_sushi_iwata = Restaurante('Iwata Sushi', 'Japonês')
+#restaurante_sushi_iwata.nome = 'Iwata Sushi'
+#restaurante_sushi_iwata.categoria = 'Japones'
+restaurante_jorge_boteco = Restaurante('Boteco do Jorge','Brasileira')
+
+Restaurante.listar_restaurantes()

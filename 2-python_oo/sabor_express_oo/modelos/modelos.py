7c142050cf53f5858cf8411c7f6cc7dd1b2a0444
Hellen Iwata <hellen.iwata001@gmail.com>
2024-01-26 01:47:18 -0300

    Adicionando o decorador @property, ajustando a saída da função listar_restaurante com o ljust, tornando o atributo nome e ativo como protegido e adicionando comentários sobre a definição dos métodos especiais, funções e decorador: __init__, __str__, .title(), @property

--- 2-python_oo/sabor_express_oo/modelos/modelos.py
+++ 2-python_oo/sabor_express_oo/modelos/modelos.py
@@ -1,21 +1,30 @@
 class Restaurante:
     restaurantes = []
     
+    # Método __init__: é uma função especial que podemos aplicar para iniciar a classe. Ao criar o objeto da classe, o método é aplicado automaticamente e define o comportamento inicial do objeto. 
+    # Sintaxe: def __init__(self, *args, **kwargs) onde temos:
+        #*args: Utilizado para passar argumentos de construção para a classe, se não houve pode utilizar 'none'
+        #**kwargs: Utilizado para passar parâmetros de inicialização para a classe quando fazemos o input de um objeto    
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title() #.title(): Função que retorna obrigatóriamente a primeira letra do texto em maiúscula, funcionamento parecido com o '.upper()'
+        self.categoria = categoria.title()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
+    #Método __str__: é uma função especial utilizado para retornar uma representação de texto
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restaurantes():
-        print('Nome | Categoria | Status')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status\n')
         for restaurante in Restaurante.restaurantes:
             
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
-            
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+    
+    # Decorador @property: utilizado para transformar um método em uma propriedade de uma classe. Ele permite que um método seja acessado como atributo, sem a necessidade de chamá-lo como uma função.
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'            
 
 
 restaurante_sushi_iwata = Restaurante('Iwata Sushi', 'Japonês')
@@ -24,3 +33,4 @@ restaurante_sushi_iwata = Restaurante('Iwata Sushi', 'Japonês')
 restaurante_jorge_boteco = Restaurante('Boteco do Jorge','Brasileira')
 
 Restaurante.listar_restaurantes()
+
